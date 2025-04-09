5181297c8f1b1470039fb543991e78efda865a29
Kim Kudsk <kim.kudsk@gmail.com>
2025-04-10 00:08:39 +0200

    Add first text and README

---
+++ README.md
@@ -0,0 +1,15 @@
+# BPE
+
+This is my version of BPE from Tsoding.
+
+## Quick Start
+
+```console
+$ cc -o nob nob.c
+$ ./nob
+```
+
+## References
+
+- https://github.com/tsoding/bpe
+- https://en.wikipedia.org/wiki/Byte_pair_encoding
--- bpe.c
+++ bpe.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
+#include <string.h>
 
-int main(int argc, char *argv[]) {
-  printf("Hello, world");
+int main() {
+  const char *text =
+      "The original BPE algorithm operates by iteratively replacing the most "
+      "common contiguous sequences of characters in a target text with unused "
+      "'placeholder' bytes. The iteration ends when no sequences can be found, "
+      "leaving the target text effectively compressed. Decompression can be "
+      "performed by reversing this process, querying known placeholder terms "
+      "against their corresponding denoted sequence, using a lookup table. In "
+      "the original paper, this lookup table is encoded and stored alongside "
+      "the compressed text.";
+
+  int text_size = strlen(text);
+
+  for (int i = 0; i < text_size - 1; ++i) {
+    char a = text[i];
+    char b = text[i + 1];
+    printf("%c%c\n", a, b);
+  }
+
   return 0;
 }
